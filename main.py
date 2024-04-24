54afad6ee6d793a11925894376701a63f8c99bed
mehmetdogukanozer <146479678+mehmetdogukanozer@users.noreply.github.com>
2024-04-24 18:47:52 +0300

    main py cofs done

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from src.word_functions import createSecret
 from src.api import collect_word
 from src.player import Player
+
+def main()->None:
+    player_zodiac = Player()
+
+    while(True):
+
+        word = collect_word()
+        secret = createSecret(word , 2)
+
+
+        print(f"secret : {secret}")
+
+        guessed_word = input("guess secret word: ")
+        if guessed_word == word:
+            print("you win")
+            break
+        else:
+            player_zodiac.lose_hp()
+            print(f"your current hp: {player_zodiac.hp}")
+
+        if player_zodiac.hp == 0:
+            print("Game Over you lose")
+            break
+        
+        
+if __name__ == "__main__":
+    main()
